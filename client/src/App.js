407610db36a58bc96e71131b4b1fea87c0938eba
Stav <stavush93@gmail.com>
2022-03-14 22:02:01 +0200

    routes component

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import MainHeader from "./components/UI/MainHeader/MainHeader.js"
-import Login from './components/User/Login/Login.js';
-// import Home from './components/UI/Home/Home.js';
-
+import AppRoutes from "./components/UI/AppRoutes.js"
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // useEffect(() => {
-  //   const storedUserLoggedInInformaition = localStorage.getItem('isLoggedIn')
-
-  //   if (storedUserLoggedInInformaition === "1") {
-  //     setIsLoggedIn(true);
-  //   }
-  // },[])
-
-  // const loginHandler = (name, email, password) => {
-  //   // We should of course check email and password
-  //   // But it's just a dummy/ demo anyways
-  //   localStorage.setItem("isLoggedIn","1")
-  //   setIsLoggedIn(true);
-  // };
-
-  // const logoutHandler = () => {
-  //   setIsLoggedIn(false);
-  // };
 
   return (
     <React.Fragment>
       <MainHeader/>
-      <Login/>
-      {/* <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main> */}
+      <AppRoutes/>
     </React.Fragment>
   );
 }
 
 export default App;
+
---
+++ client/src/components/UI/AppRoutes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Login from '../User/Login/Login.js';
+import { Route, Routes } from 'react-router-dom';
+import UserProfile from '../User/UserProfile/UserProfile.js';
+import AddDog from '../User/Dogs/AddDog/AddDog.js';
+import ParksList from '../Parks/ParksList/ParksList.js';
+import Park from '../Parks/Park/Park.js';
+
+function AppRoutes() {
+
+  return (
+    <Routes>
+      {/* לוגין */}
+      <Route path='/login' element={(<Login/>)} /> 
+    
+      {/* פרופיל */}
+      <Route path='/profile/:profileName' element={( <UserProfile />)}/>
+      <Route path='/profile/:profileName/:dogName' element={(<AddDog/>)} />
+          
+      {/* גינות */}
+      <Route path='/parks' element={(<ParksList />)}/>
+      <Route path='/parks/:parkName' element={(<Park/>)} /> 
+    </Routes>
+  )
+}
+
+export default AppRoutes
